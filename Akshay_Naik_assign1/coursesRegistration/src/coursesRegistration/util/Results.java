8b2a603b88c630e85e77431516981ddee7d7fbfe
Axe2211 <anaik4@binghmton.edu>
2019-09-20 23:48:48 -0400

    Implements the logic fors writeToStdOut and writeToFile methods

--- Akshay_Naik_assign1/coursesRegistration/src/coursesRegistration/util/Results.java
+++ Akshay_Naik_assign1/coursesRegistration/src/coursesRegistration/util/Results.java
@@ -1,6 +1,59 @@
 package coursesRegistration.util;
 
+import coursesRegistration.inputds.StudentPool;
+import coursesRegistration.scheduler.CourseAllocation;
+import coursesRegistration.inputds.Student;
+import java.util.Iterator;
+import java.io.PrintWriter;
+import java.io.File;
+
 
 public class Results implements FileDisplayInterface, StdoutDisplayInterface {
 	
+    public void writeToFile(CourseAllocation allocation, String outputFileName){
+ 
+        Iterator studentItr = allocation.getStudents().getStudentList().iterator();
+        Student student;
+
+        try{
+            File outputFile = new File(outputFileName);
+            outputFile.createNewFile();
+            PrintWriter outputFileHandler = new PrintWriter(outputFile);
+            while(studentItr.hasNext()){
+                    student = (Student)studentItr.next();
+                    outputFileHandler.println(new String(student.getStudentId() + 
+                                    ": " + student.getAllocatedCourses()[0].getCourseName() +
+                                    ", " + student.getAllocatedCourses()[1].getCourseName() +
+                                    ", " + student.getAllocatedCourses()[2].getCourseName() +
+                                    "::SatisfactionRating=" + student.getSatisfactionIndex()));
+            }
+            outputFileHandler.println(new String("AverageSatisfactionRating=" + allocation.getAvgSatisfaction()));
+            outputFileHandler.close();
+        }
+        catch(Exception e){
+            System.out.println(" Error in writing to file.. ");
+        }
+    }
+
+    public void writeToStdOut(CourseAllocation allocation){
+
+        Iterator studentItr = allocation.getStudents().getStudentList().iterator();
+        Student student;
+
+        try{
+            while(studentItr.hasNext()){
+                    student = (Student)studentItr.next();
+                    System.out.println(new String(student.getStudentId() + 
+                                    ": " + student.getAllocatedCourses()[0].getCourseName() +
+                                    ", " + student.getAllocatedCourses()[1].getCourseName() +
+                                    ", " + student.getAllocatedCourses()[2].getCourseName() +
+                                    "::SatisfactionRating=" + student.getSatisfactionIndex()));
+            }
+            System.out.println(new String("AverageSatisfactionRating=" + allocation.getAvgSatisfaction()));
+        }
+        catch(Exception e){
+            System.out.println(" Error in writing to standard display.. ");
+        }
+    }
 }
+ 
