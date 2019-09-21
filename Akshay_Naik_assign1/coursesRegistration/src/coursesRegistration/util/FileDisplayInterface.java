ac73e647c708f06250ea6478bf2913c60806b033
Axe2211 <anaik4@binghmton.edu>
2019-09-20 23:47:04 -0400

    Interface contains writeToFile method declaration

--- Akshay_Naik_assign1/coursesRegistration/src/coursesRegistration/util/FileDisplayInterface.java
+++ Akshay_Naik_assign1/coursesRegistration/src/coursesRegistration/util/FileDisplayInterface.java
@@ -1,5 +1,10 @@
 package coursesRegistration.util;
 
+import coursesRegistration.scheduler.CourseAllocation;
+import coursesRegistration.util.FileProcessor;
+
 public interface FileDisplayInterface {
+
+    public void writeToFile(CourseAllocation allocater, String outputFile);
 	
 }
